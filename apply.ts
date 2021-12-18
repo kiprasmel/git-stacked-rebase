cd467952c709ca89a8b95d6f31c9b13191ae6b9d
Kipras Melnikovas <kipras@kipras.org>
2021-12-19 00:31:00 +0200

    refactor: extract `parseNewGoodCommands` fn
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- apply.ts
+++ apply.ts
@@ -1,16 +1,13 @@
-/* eslint-disable indent */
-
 import fs from "fs";
-import path from "path";
 import assert from "assert";
 
 import Git from "nodegit";
-import { array } from "nice-comment";
 
-import { parseTodoOfStackedRebase } from "./parse-todo-of-stacked-rebase/parseTodoOfStackedRebase";
-import { GoodCommand, stackedRebaseCommands } from "./parse-todo-of-stacked-rebase/validator";
+import { GoodCommand } from "./parse-todo-of-stacked-rebase/validator";
 import { createExecSyncInRepo } from "./util/execSyncInRepo";
 import { EitherExitFinal, fail } from "./util/Exitable";
+
+import { parseNewGoodCommands } from "./parse-todo-of-stacked-rebase/parseNewGoodCommands";
 
 export type ApplyArgs = {
 	pathToStackedRebaseDirInsideDotGit: string; //
@@ -29,146 +26,8 @@ export async function apply({
 		return fail("\n\nno stacked-rebase in progress? (nothing to --apply)\n\n");
 	}
 
-	const [exit, goodCommands] = parseTodoOfStackedRebase(pathToStackedRebaseTodoFile);
-	if (!goodCommands) return fail(exit);
-
-	logGoodCmds(goodCommands);
-
-	const pathOfRewrittenList: string = path.join(repo.workdir(), ".git", "stacked-rebase", "rewritten-list");
-	const rewrittenList: string = fs.readFileSync(pathOfRewrittenList, { encoding: "utf-8" });
-	const rewrittenListLines: string[] = rewrittenList.split("\n").filter((line) => !!line);
-
-	console.log({ rewrittenListLines });
-
-	const newCommits: { newSHA: string; oldSHAs: string[] }[] = [];
-
-	type OldCommit = { oldSHA: string; newSHA: string; changed: boolean };
-	const oldCommits: OldCommit[] = [];
-
-	rewrittenListLines.map((line) => {
-		const fromToSHA = line.split(" ");
-		assert(
-			fromToSHA.length === 2,
-			"from and to SHAs, coming from rewritten-list, are written properly (1 space total)."
-		);
-
-		const [oldSHA, newSHA] = fromToSHA;
-
-		oldCommits.push({ oldSHA, newSHA, changed: oldSHA !== newSHA });
-
-		const last = newCommits.length - 1;
-
-		if (newCommits.length && newSHA === newCommits[last].newSHA) {
-			newCommits[last].oldSHAs.push(oldSHA);
-		} else {
-			newCommits.push({
-				newSHA,
-				oldSHAs: [oldSHA],
-			});
-		}
-
-		//
-	});
-
-	console.log({ newCommits: newCommits.map((c) => c.newSHA + ": " + array(c.oldSHAs)) });
-	console.log({ oldCommits });
-
-	/**
-	 * match oldCommits & goodCommands
-	 */
-	const goodNewCommands: GoodCommand[] = [];
-
-	goodNewCommands.push(goodCommands[0]);
-
-	let lastNewCommit: OldCommit | null = null;
-
-	let goodCommandMinIndex = 1;
-	for (let i = 0; i < oldCommits.length; i++) {
-		const oldCommit: OldCommit = oldCommits[i];
-
-		const oldCommandAtIdx: GoodCommand = goodCommands[goodCommandMinIndex];
-
-		if (oldCommandAtIdx.commandName in stackedRebaseCommands) {
-			goodNewCommands.push({
-				...oldCommandAtIdx,
-				commitSHAThatBranchPointsTo: (lastNewCommit as OldCommit | null)?.newSHA ?? null, // TODO TS
-			} as any); // TODO TS
-			goodCommandMinIndex++;
-		}
-
-		const goodOldCommand = goodCommands.find((cmd) => cmd.targets?.[0] === oldCommit.oldSHA);
-
-		if (!goodOldCommand) {
-			throw new Error("TODO: goodCommandOld not found");
-		}
-
-		const update = () => {
-			if (goodOldCommand.commandName in stackedRebaseCommands) {
-				// do not modify
-				/** TODO FIXME CLEANUP: this actually never happens: (add `assert(false)`) */
-				goodNewCommands.push(goodOldCommand);
-				// goodCommandMinIndex++;
-			} else {
-				// goodNewCommands.push({ ...goodOldCommand, targets: [oldCommit.newSHA] /** TODO VERIFY */ });
-				lastNewCommit = oldCommit;
-				goodNewCommands.push({ ...goodOldCommand, targets: [oldCommit.newSHA] /** TODO VERIFY */ });
-				goodCommandMinIndex++;
-			}
-		};
-
-		if (goodOldCommand.index < goodCommandMinIndex) {
-			// TODO VERIFY
-			console.warn(
-				`goodCommandOld.index (${goodOldCommand.index}) < goodCommandMinIndex (${goodCommandMinIndex}), continue'ing.`
-			);
-
-			// goodCommandMinIndex++;
-
-			continue;
-		} else if (goodOldCommand.index === goodCommandMinIndex) {
-			// perfect?
-			// TODO VERIFY
-			console.info(`index match`);
-
-			update();
-		} else {
-			// jump?
-			// TODO VERIFY
-			console.warn(`jump, continue'ing`);
-
-			// update(); // TODO VERIFY
-			continue;
-		}
-
-		//
-	}
-
-	goodNewCommands.push(goodCommands[goodCommands.length - 1]);
-
-	// console.log({ goodNewCommands });
-	console.log({
-		len: goodCommands.length,
-		goodCommands: goodCommands.map((c) => c.commandOrAliasName + ": " + c.targets?.join(", ") + "."),
-	});
-
-	console.log({
-		len: goodNewCommands.length,
-		goodNewCommands: goodNewCommands.map((c) => c.commandOrAliasName + ": " + c.targets?.join(", ") + "."),
-	});
-
-	const stackedRebaseCommandsOld = goodCommands.filter((cmd) => cmd.commandName in stackedRebaseCommands);
-	const stackedRebaseCommandsNew = goodNewCommands
-		.map((cmd, i) =>
-			cmd.commandName in stackedRebaseCommands
-				? {
-						...cmd,
-						commitSHAThatBranchPointsTo: i > 0 ? goodNewCommands[i - 1].targets?.[0] ?? null : null,
-				  }
-				: false
-		)
-		.filter((cmd) => !!cmd);
-
-	assert(stackedRebaseCommandsOld.length === stackedRebaseCommandsNew.length);
+	const [exit, stackedRebaseCommandsNew] = parseNewGoodCommands(repo, pathToStackedRebaseTodoFile);
+	if (!stackedRebaseCommandsNew) return fail(exit);
 
 	// const remotes: Git.Remote[] = await repo.getRemotes();
 	// const remote: Git.Remote | undefined = remotes.find((r) =>
@@ -288,16 +147,3 @@ export async function apply({
 
 	return;
 }
-
-const logGoodCmds = (goodCommands: GoodCommand[]): void => {
-	console.log({
-		goodCommands: goodCommands.map((c) => ({
-			...c,
-			targets: c.targets?.length === 1 ? c.targets[0] : array(c.targets ?? []),
-		})),
-	});
-
-	console.log({
-		goodCommands: goodCommands.map((c) => c.commandOrAliasName + ": " + c.targets?.join(", ") + "."),
-	});
-};
---
+++ parse-todo-of-stacked-rebase/parseNewGoodCommands.ts
@@ -0,0 +1,174 @@
+/* eslint-disable indent */
+
+import fs from "fs";
+import path from "path";
+import assert from "assert";
+
+import Git from "nodegit";
+import { array } from "nice-comment";
+
+import { EitherExit, fail, succ } from "../util/Exitable";
+
+import { parseTodoOfStackedRebase } from "./parseTodoOfStackedRebase";
+import { GoodCommand, stackedRebaseCommands } from "./validator";
+
+export function parseNewGoodCommands(
+	repo: Git.Repository,
+	pathToStackedRebaseTodoFile: string //
+): EitherExit<GoodCommand[]> {
+	const [exit, goodCommands] = parseTodoOfStackedRebase(pathToStackedRebaseTodoFile);
+	if (!goodCommands) return fail(exit);
+
+	logGoodCmds(goodCommands);
+
+	const pathOfRewrittenList: string = path.join(repo.workdir(), ".git", "stacked-rebase", "rewritten-list");
+	const rewrittenList: string = fs.readFileSync(pathOfRewrittenList, { encoding: "utf-8" });
+	const rewrittenListLines: string[] = rewrittenList.split("\n").filter((line) => !!line);
+
+	console.log({ rewrittenListLines });
+
+	const newCommits: { newSHA: string; oldSHAs: string[] }[] = [];
+
+	type OldCommit = { oldSHA: string; newSHA: string; changed: boolean };
+	const oldCommits: OldCommit[] = [];
+
+	rewrittenListLines.map((line) => {
+		const fromToSHA = line.split(" ");
+		assert(
+			fromToSHA.length === 2,
+			"from and to SHAs, coming from rewritten-list, are written properly (1 space total)."
+		);
+
+		const [oldSHA, newSHA] = fromToSHA;
+
+		oldCommits.push({ oldSHA, newSHA, changed: oldSHA !== newSHA });
+
+		const last = newCommits.length - 1;
+
+		if (newCommits.length && newSHA === newCommits[last].newSHA) {
+			newCommits[last].oldSHAs.push(oldSHA);
+		} else {
+			newCommits.push({
+				newSHA,
+				oldSHAs: [oldSHA],
+			});
+		}
+
+		//
+	});
+
+	console.log({ newCommits: newCommits.map((c) => c.newSHA + ": " + array(c.oldSHAs)) });
+	console.log({ oldCommits });
+
+	/**
+	 * match oldCommits & goodCommands
+	 */
+	const goodNewCommands: GoodCommand[] = [];
+
+	goodNewCommands.push(goodCommands[0]);
+
+	let lastNewCommit: OldCommit | null = null;
+
+	let goodCommandMinIndex = 1;
+	for (let i = 0; i < oldCommits.length; i++) {
+		const oldCommit: OldCommit = oldCommits[i];
+
+		const oldCommandAtIdx: GoodCommand = goodCommands[goodCommandMinIndex];
+
+		if (oldCommandAtIdx.commandName in stackedRebaseCommands) {
+			goodNewCommands.push({
+				...oldCommandAtIdx,
+				commitSHAThatBranchPointsTo: (lastNewCommit as OldCommit | null)?.newSHA ?? null, // TODO TS
+			} as any); // TODO TS
+			goodCommandMinIndex++;
+		}
+
+		const goodOldCommand = goodCommands.find((cmd) => cmd.targets?.[0] === oldCommit.oldSHA);
+
+		if (!goodOldCommand) {
+			throw new Error("TODO: goodCommandOld not found");
+		}
+
+		const update = () => {
+			if (goodOldCommand.commandName in stackedRebaseCommands) {
+				// do not modify
+				/** TODO FIXME CLEANUP: this actually never happens: (add `assert(false)`) */
+				goodNewCommands.push(goodOldCommand);
+				// goodCommandMinIndex++;
+			} else {
+				// goodNewCommands.push({ ...goodOldCommand, targets: [oldCommit.newSHA] /** TODO VERIFY */ });
+				lastNewCommit = oldCommit;
+				goodNewCommands.push({ ...goodOldCommand, targets: [oldCommit.newSHA] /** TODO VERIFY */ });
+				goodCommandMinIndex++;
+			}
+		};
+
+		if (goodOldCommand.index < goodCommandMinIndex) {
+			// TODO VERIFY
+			console.warn(
+				`goodCommandOld.index (${goodOldCommand.index}) < goodCommandMinIndex (${goodCommandMinIndex}), continue'ing.`
+			);
+
+			// goodCommandMinIndex++;
+
+			continue;
+		} else if (goodOldCommand.index === goodCommandMinIndex) {
+			// perfect?
+			// TODO VERIFY
+			console.info(`index match`);
+
+			update();
+		} else {
+			// jump?
+			// TODO VERIFY
+			console.warn(`jump, continue'ing`);
+
+			// update(); // TODO VERIFY
+			continue;
+		}
+
+		//
+	}
+
+	goodNewCommands.push(goodCommands[goodCommands.length - 1]);
+
+	// console.log({ goodNewCommands });
+	console.log({
+		len: goodCommands.length,
+		goodCommands: goodCommands.map((c) => c.commandOrAliasName + ": " + c.targets?.join(", ") + "."),
+	});
+
+	console.log({
+		len: goodNewCommands.length,
+		goodNewCommands: goodNewCommands.map((c) => c.commandOrAliasName + ": " + c.targets?.join(", ") + "."),
+	});
+
+	const stackedRebaseCommandsOld = goodCommands.filter((cmd) => cmd.commandName in stackedRebaseCommands);
+	const stackedRebaseCommandsNew: GoodCommand[] = goodNewCommands
+		.map((cmd, i) =>
+			cmd.commandName in stackedRebaseCommands
+				? {
+						...cmd,
+						commitSHAThatBranchPointsTo: i > 0 ? goodNewCommands[i - 1].targets?.[0] ?? null : null,
+				  }
+				: false
+		)
+		.filter((cmd) => !!cmd) as GoodCommand[]; // TODO TS should infer automatically
+
+	assert(stackedRebaseCommandsOld.length === stackedRebaseCommandsNew.length);
+
+	return succ(stackedRebaseCommandsNew);
+}
+
+const logGoodCmds = (goodCommands: GoodCommand[]): void => {
+	console.log({
+		goodCommands: goodCommands.map((c) => ({
+			...c,
+			targets: c.targets?.length === 1 ? c.targets[0] : array(c.targets ?? []),
+		})),
+	});
+
+	console.log({
+		goodCommands: goodCommands.map((c) => c.commandOrAliasName + ": " + c.targets?.join(", ") + "."),
+	});
+};
