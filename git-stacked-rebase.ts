7ebafb5ccecc0cc74f96c9ca436e8b480b56b73c
Kipras Melnikovas <kipras@kipras.org>
2022-03-18 19:54:25 +0200

    add the current commit's SHA to the rewritten list
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- git-stacked-rebase.ts
+++ git-stacked-rebase.ts
@@ -647,6 +647,7 @@ REWRITTEN_LIST_BACKUP_FILE_PATH="$STACKED_REBASE_DIR/${filenames.rewrittenList}"
 #EOF
 
 cat >> "$REWRITTEN_LIST_BACKUP_FILE_PATH" <<EOF
+$(git rev-parse HEAD)
 $(cat /dev/stdin)
 
 EOF
