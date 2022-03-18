7138c2b3665d17d187b7e0c5aa1975c362ad786f
Kipras Melnikovas <kipras@kipras.org>
2022-03-18 02:05:54 +0200

    find the (bug?) in git's post-rewrite hook, attempt initial fix by merging `amend`s & `rebase`s, figure out why fix is not sufficient for us yet
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- reduce-path.js
+++ reduce-path.js
@@ -52,6 +52,15 @@ function reducePath(obj) {
 			}
 		}
 	}
+
+	/**
+	 * we mutate the object, so NOT returning it makes it clear
+	 * that this function causes a side-effect (mutates the original object).
+	 * 
+	 * but, in multiple cases when mapping, we forget to return the object,
+	 * so instead we'll do it here:
+	 */
+	return obj
 }
 
 const prefix = "" // "test/.tmp-described.off/"
@@ -76,6 +85,57 @@ const rewrittenLists = rewrittenListFile
 console.log({ rewrittenLists });
 
 const reducedRewrittenLists = rewrittenLists
-	.map(list => (reducePath(list), list))
+	// .map(list => (reducePath(list), list))
+	.map(list => reducePath(list))
 
 console.log({ reducedRewrittenLists });
+
+const last = reducedRewrittenLists[reducedRewrittenLists.length - 1]
+const lastVals = Object.values(last).reverse()
+console.log({ lastVals });
+/**
+ * compare with
+ * git log --pretty=format:"%H"
+ */
+
+/**
+ * ayo lol, prolly found a bug in git -- the output in
+ * rewritten-list, or /dev/stdin, after the rebase is done,
+ * is not fully complete -- it doesn't correctly print the SHAs
+ * that got changed while the rebase was paused,
+ * e.g. commit --amend while paused by "edit" or "break" commands.
+ * 
+ * thus, at least until we report it and confirm it's actually a bug
+ * and not intended this way,
+ * we can get the desired output for ourselves
+ * by merging all the rewrittenLists!
+ * 
+ */
+
+/** 
+ * TODO verify keys/values are not identical in a bad way
+ */
+const merge = (A, B) => ({
+	...A,
+	...B
+})
+
+const mergedReducedRewrittenLists = reducedRewrittenLists.reduce((acc, curr) => reducePath(merge(acc, curr)), {})
+
+console.log({ mergedReducedRewrittenLists });
+const vals = Object.values(mergedReducedRewrittenLists).reverse()
+console.log({ vals });
+
+/**
+ * it fixes the above issues!
+ * 
+ * but wait! we cannot just merge like this,
+ * because when we take the values,
+ * the commits whom were not the full list (and only 1 commit remap, because --amend,
+ * or not full rebase up till the initial branch (TODO need to confirm the 2nd case)),
+ * end up being in the wrong place (they end up either in the start or the end).
+ * 
+ * the problem is that we don't know __when__ the rewrite of the commit happened.
+ * TODO need to track that as well?
+ * 
+ */
