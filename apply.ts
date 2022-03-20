56b834e5bd6e9ce82f4c77c247bcf97f3225e7dd
Kipras Melnikovas <kipras@kipras.org>
2022-03-20 03:42:40 +0200

    strongly refactor branchSequencer to work with simple boundaries (branch & commit)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- apply.ts
+++ apply.ts
@@ -4,7 +4,6 @@ import path from "path";
 import Git from "nodegit";
 
 import { createQuestion } from "./util/createQuestion";
-import { noop } from "./util/noop";
 
 import { filenames } from "./filenames";
 import { configKeys } from "./configKeys";
@@ -13,7 +12,6 @@ import {
 	BranchSequencerBase, //
 	branchSequencer,
 	ActionInsideEachCheckedOutBranch,
-	CallbackAfterDone,
 	BranchSequencerArgsBase,
 } from "./branchSequencer";
 import { combineRewrittenLists } from "./reducePath";
@@ -32,13 +30,12 @@ const defaultApplyAction: ActionInsideEachCheckedOutBranch = async ({
 	repo, //
 	// targetBranch,
 	targetCommitSHA,
-	cmd,
 	isFinalCheckout,
 	// execSyncInRepo,
 }) => {
 	const commit: Git.Commit = await Git.Commit.lookup(repo, targetCommitSHA);
 
-	console.log("will reset because", cmd.commandOrAliasName, "to commit", commit.summary(), commit.sha());
+	console.log("will reset to commit", commit.sha(), "(" + commit.summary() + ")");
 
 	console.log({ isFinalCheckout });
 
@@ -53,36 +50,6 @@ const defaultApplyAction: ActionInsideEachCheckedOutBranch = async ({
 
 export const getBackupPathOfPreviousStackedRebase = (pathToStackedRebaseDirInsideDotGit: string): string =>
 	pathToStackedRebaseDirInsideDotGit + ".previous";
-
-/**
- * disabled because `forcePush` also became a thing
- * and it's no longer clear what marks a stacked-rebase "done",
- *
- * thus making it hard to work with the temporary/previous directories
- * without introducing a good amount of bugs.
- *
- */
-const defaultApplyCallback__disabled: CallbackAfterDone = ({
-	pathToStackedRebaseDirInsideDotGit, //
-}): void => {
-	const backupPath: string = getBackupPathOfPreviousStackedRebase(pathToStackedRebaseDirInsideDotGit);
-
-	/**
-	 * backup dir just in case, but in inactive path
-	 * (so e.g --apply won't go off again accidently)
-	 */
-	if (fs.existsSync(backupPath)) {
-		fs.rmdirSync(backupPath, { recursive: true });
-	}
-	fs.renameSync(pathToStackedRebaseDirInsideDotGit, backupPath);
-
-	// diffCommands.forEach((cmd) => {
-	// 	console.log({ cmd });
-	// 	execSyncInRepo(cmd, { ...pipestdio(repo.workdir()) });
-	// });
-	//
-};
-noop(defaultApplyCallback__disabled);
 
 export type ReturnOfApplyIfNeedsToApply = {
 	markThatNeedsToApply: () => void;
