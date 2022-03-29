d28e290275f527da985a41c1c5c315811eefe2ab
Kipras Melnikovas <kipras@kipras.org>
2022-03-29 14:58:17 +0300

    TODO FIX print relative date of when script was built
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- script/postbuild.js
+++ script/postbuild.js
@@ -6,13 +6,23 @@ const executablePath = "./dist/git-stacked-rebase.js";
 fs.chmodSync(executablePath, "755");
 execSyncP(`ls -la ${executablePath}`);
 
-updateShebang(executablePath);
-execSyncP(`cat ${executablePath} | head -n 2`);
+modifyLines(executablePath);
 
-function updateShebang(path) {
+function modifyLines(path) {
 	const file = fs.readFileSync(path, { encoding: "utf-8" });
 	const lines = file.split("\n");
 
+	const afterUpdateShebang = updateShebang(lines);
+	const afterInjectRelativeBuildDatePrinter = injectRelativeBuildDatePrinter(lines);
+
+	const newFile = lines.join("\n");
+	fs.writeFileSync(path, newFile);
+
+	afterUpdateShebang();
+	afterInjectRelativeBuildDatePrinter();
+}
+
+function updateShebang(lines) {
 	const oldShebang = "#!/usr/bin/env ts-node-dev";
 	const newShebang = "#!/usr/bin/env node";
 
@@ -22,8 +32,23 @@ function updateShebang(path) {
 		lines.splice(0, 0, newShebang);
 	}
 
-	const newFile = lines.join("\n");
-	fs.writeFileSync(path, newFile);
+	return () => execSyncP(`cat ${executablePath} | head -n 2`);
+}
 
-	return;
+function injectRelativeBuildDatePrinter(lines) {
+	const BUILD_DATE_REPLACEMENT_STR = "__BUILD_DATE_REPLACEMENT_STR__";
+	const targetLineIdx = lines.findIndex((line) => line.includes(BUILD_DATE_REPLACEMENT_STR));
+	const buildDate = new Date().getTime();
+	const printRelativeDate =
+		"(" + //
+		"built " +
+		"${" +
+		`Math.round((new Date() - ${buildDate}) / 1000 / 60)` +
+		"}" +
+		" mins ago" +
+		")";
+
+	lines[targetLineIdx] = lines[targetLineIdx].replace(BUILD_DATE_REPLACEMENT_STR, printRelativeDate);
+
+	return () => execSyncP(`cat ${executablePath} | grep " mins ago"`);
 }
