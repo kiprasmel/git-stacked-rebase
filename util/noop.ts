d3e8edad40595c87584915ab6e111b88829efb76
Kipras Melnikovas <kipras@kipras.org>
2022-04-02 16:15:08 +0300

    explain the `noop` function
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- util/noop.ts
+++ util/noop.ts
@@ -1,3 +1,8 @@
+/**
+ * a simple function that does nothing (no-operation),
+ * used to prevent compiler errors for unused code.
+ * especially useful if you're experimenting temporarily.
+ */
 export function noop(..._xs: any[]): void {
 	//
 }
