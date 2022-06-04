d517ea53ef766adc3d842152587823cdc7c511a0
Kipras Melnikovas <kipras@kipras.org>
2022-06-05 01:50:41 +0300

    fix: support explicit drop command
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- parse-todo-of-stacked-rebase/parseNewGoodCommands.spec.ts
+++ parse-todo-of-stacked-rebase/parseNewGoodCommands.spec.ts
@@ -4,6 +4,7 @@ import { gitStackedRebase } from "../git-stacked-rebase";
 import {
 	humanOpAppendLineAfterNthCommit, //
 	humanOpRemoveLineOfCommit,
+	humanOpChangeCommandOfNthCommitInto,
 } from "../humanOp";
 
 import { setupRepoWithStackedBranches } from "../test/setupRepo";
@@ -11,6 +12,7 @@ import { setupRepoWithStackedBranches } from "../test/setupRepo";
 export async function parseNewGoodCommandsSpec() {
 	await succeeds_to_apply_after_break_or_exec();
 	await succeeds_to_apply_after_implicit_drop();
+	await succeeds_to_apply_after_explicit_drop();
 
 	async function succeeds_to_apply_after_break_or_exec() {
 		const { initialBranch, commitOidsInLatestStacked, dir, config } = await setupRepoWithStackedBranches();
@@ -52,6 +54,32 @@ export async function parseNewGoodCommandsSpec() {
 			editor: ({ filePath }) => {
 				const commitSHA: string = commitOidsInLatestStacked[7].tostrS();
 				humanOpRemoveLineOfCommit({ filePath, commitSHA });
+			},
+		});
+
+		await gitStackedRebase(branch, {
+			...common,
+			apply: true,
+		});
+	}
+
+	async function succeeds_to_apply_after_explicit_drop(): Promise<void> {
+		const { initialBranch, commitOidsInLatestStacked, dir, config } = await setupRepoWithStackedBranches();
+		const branch = initialBranch.shorthand();
+
+		const common = {
+			gitDir: dir,
+			getGitConfig: () => config,
+		} as const;
+
+		await gitStackedRebase(branch, {
+			...common,
+			editor: ({ filePath }) => {
+				const commitSHA: string = commitOidsInLatestStacked[7].tostrS();
+				humanOpChangeCommandOfNthCommitInto("drop", {
+					filePath,
+					commitSHA,
+				});
 			},
 		});
 
