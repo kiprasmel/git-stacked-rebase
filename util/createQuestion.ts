a32f15c0153855ebd5422c6fb0a278a0fceba6d9
Kipras Melnikovas <kipras@kipras.org>
2023-04-02 13:34:45 +0300

    refactor `applyIfNeedsToApply` logic
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- util/createQuestion.ts
+++ util/createQuestion.ts
@@ -1,12 +1,17 @@
 import readline from "readline";
 
-export const createQuestion = (
-	rl = readline.createInterface(process.stdin, process.stdout) //
-) => (
-	q: string //
-): Promise<string> =>
-	new Promise<string>((r) =>
-		rl.question(q, (ans) => {
-			r(ans);
-		})
-	);
+export const createQuestion =
+	(
+		rl = readline.createInterface(process.stdin, process.stdout) //
+	) =>
+	(
+		q: string //
+	): Promise<string> =>
+		new Promise<string>((r) =>
+			rl.question(q, (ans) => {
+				r(ans);
+			})
+		);
+
+export const question = (q: string, cb: (ans: string) => string, { prefix = "\n" } = {}): Promise<string> =>
+	createQuestion()(prefix + q).then(cb);
