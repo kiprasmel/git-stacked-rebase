cbe8085a09abad46fefb315f0e5186d829c3c4ce
Kipras Melnikovas <kipras@kipras.org>
2023-04-29 18:03:04 +0300

    TEMP

--- ref-finder.sh
+++ ref-finder.sh
@@ -8,9 +8,31 @@ LATEST_BRANCH_COMMIT="$(git rev-parse "$LATEST_BRANCH")"
 
 printf "initial branch:\n$INITIAL_BRANCH_COMMIT\n\n"
 
+# TODO:
+# 1. go thru all version of latest branch in the stack, to check if 
+#    lost partial branch has pointed to it, just a previous version
+#   (means still in the stack, highly likely).
+#
+# TODO:
+# 2. go thru all versions of all partial branches in the stack,
+#    to check if lost partial branch has pointed to any of them,
+#    just a previous version
+#    (still somewhat likely that still belongs to the stack).
+#
+# TODO:
+# 3. cache data for 1. and later 2., so that it doesn't take forever to compute.
+#
+# 
+
 git for-each-ref --format='%(objectname) %(objecttype) %(refname)' | node -pe "
 fs = require('fs')
 cp = require('child_process')
+
+const ignoreTags = x => x.objtype !== 'tag'
+const ignoreTagLike = x => !x.refname.startsWith('refs/tags/')
+const ignoreOutsideStack = x => x.ref_exists_between_latest_and_initial
+
+const REF_PASSES_FILTER = (x) => ignoreTags(x) && ignoreTagLike(x) && ignoreOutsideStack(x)
 
 REF_DATA = fs.readFileSync(0).toString().split('\n').slice(0, -1).map(x => x.split(' ')).map(x => {
 	const mergeBase = (a, b) => cp.execSync(\`git merge-base \${a} \${b}\`, {encoding: 'utf-8'}).trim();
@@ -26,9 +48,7 @@ REF_DATA = fs.readFileSync(0).toString().split('\n').slice(0, -1).map(x => x.spl
 		merge_base_to_latest_to_initial === \"$INITIAL_BRANCH_COMMIT\"
 		&& merge_base_to_latest !== \"$INITIAL_BRANCH_COMMIT\" /** if merge base from ref to latest is initial, then ref does not exist inside latest. */
 
-	process.stdout.write(merge_base_to_initial + ' ' + merge_base_to_latest + '\n')
-
-	return {
+	const ref = {
 		commit: x[0],
 		objtype: x[1],
 		refname: x[2],
@@ -38,20 +58,25 @@ REF_DATA = fs.readFileSync(0).toString().split('\n').slice(0, -1).map(x => x.spl
 		merge_base_to_latest_to_initial,
 		ref_exists_between_latest_and_initial,
 	}
+
+	process.stdout.write(merge_base_to_initial + ' ' + merge_base_to_latest_to_initial + ' ' + merge_base_to_latest + ' ' + REF_PASSES_FILTER(ref) + '\n')
+
+	return ref
 })
-	.filter(x => x.objtype !== 'tag') // ignore tags
-	// .filter(x => x.merge_base_to_initial_is_initial_branch) // ignore branches where merge base is not our target
-	//.filter(x => !x.ref_exists_between_latest_and_initial) // ignore branches where merge base is not our target
+	.filter(REF_PASSES_FILTER)
+
+	//.filter(x => !x.ref_exists_between_latest_and_initial) // test
+	//.filter(x => x.merge_base_to_initial_is_initial_branch && !x.ref_exists_between_latest_and_initial) // test
 
 // console.log(REF_DATA.map(x => Object.values(x).join(' ')))
-console.log(REF_DATA)
+//console.log(REF_DATA)
 
 console.log(REF_DATA.length)
 
 _ = require('lodash')
 
 REF_DATA_BY_COMMIT = _.groupBy(REF_DATA, 'commit')
-//console.log(REF_DATA_BY_COMMIT)
+console.log(REF_DATA_BY_COMMIT)
 
 //COMMIT_DATA_IN_LATEST_BRANCH = 
 
